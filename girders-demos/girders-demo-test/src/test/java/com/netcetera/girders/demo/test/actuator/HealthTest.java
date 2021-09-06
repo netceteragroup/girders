7ac4e520045b037f5ca67b2ec4ae957de0bb4285
gbrnca <86108735+gbrnca@users.noreply.github.com>
2021-09-06 15:54:29 +0200

    Update HealthTest.java
    fix Spring context for test

--- girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
+++ girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,7 +22,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,
     classes = com.netcetera.girders.demo.showcase.GirdersDemoShowcaseApplication.class,
     properties = "girders.crypto.text=disabled")
-@ImportAutoConfiguration(TestConfiguration.class)
+@Import(TestConfiguration.class)
 @ExtendWith(SpringExtension.class)
 class HealthTest {
 
