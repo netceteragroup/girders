29b17584512ff8f4949101a263a39d8714759102
Valentin Meier <valentin.meier@netcetera.com>
2022-07-29 13:39:36 +0200

    [no-issue] Re enable test

--- girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
+++ girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
@@ -2,7 +2,6 @@ package com.netcetera.girders.demo.test.actuator;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.netcetera.girders.demo.test.TestConfiguration;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,9 +33,7 @@ class HealthTest {
   @LocalServerPort
   private int port;
 
-  // TODO gbachmei 2021-09-06: re-enable test once github build is set up, fix test failure
   @Test
-  @Disabled
   void testHealth() {
     String url = "http://localhost:" + port + "/demo-showcase/actuator/health";
     JsonNode result = client.getForObject(url, JsonNode.class);
