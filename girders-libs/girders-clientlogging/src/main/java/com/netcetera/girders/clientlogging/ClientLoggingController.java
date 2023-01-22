0c40bcfb527491e44b68cfeccee28bcf47f1e621
Marija Popova <Marija.Popova@netcetera.com>
2023-01-22 16:46:39 +0100

    [26] mark client logging controller as a rest controller to be recognized by open api

--- girders-libs/girders-clientlogging/src/main/java/com/netcetera/girders/clientlogging/ClientLoggingController.java
+++ girders-libs/girders-clientlogging/src/main/java/com/netcetera/girders/clientlogging/ClientLoggingController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -16,6 +17,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  */
 @RequestMapping("/log")
 @Slf4j
+@RestController
 @Tag(name = "girders", description = "Logging")
 public class ClientLoggingController {
 
