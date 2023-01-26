82b025693881e7d4cc44139bdaceaad702381df0
Petar Pehchevski <Petar.Pehchevski@netcetera.com>
2023-01-26 11:26:47 +0100

    [22] Add embedded ldap server for testing - Add the needed dependency for embedded server - Populate the server with ldap-test.ldif

--- girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
+++ girders-demos/girders-demo-test/src/test/java/com/netcetera/girders/demo/test/actuator/HealthTest.java
@@ -2,7 +2,6 @@ package com.netcetera.girders.demo.test.actuator;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.netcetera.girders.demo.test.TestConfiguration;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,10 +22,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,
     classes = com.netcetera.girders.demo.showcase.GirdersDemoShowcaseApplication.class,
-    properties = { "girders.crypto.text=disabled", "management.health.ldap.enabled=false" })
+    properties = "girders.crypto.text=disabled")
 @Import(TestConfiguration.class)
 @ExtendWith(SpringExtension.class)
-@Slf4j
+@ActiveProfiles("health-test")
 class HealthTest {
 
   @Autowired
@@ -39,8 +39,6 @@ class HealthTest {
   void testHealth() {
     String url = "http://localhost:" + port + "/demo-showcase/actuator/health";
     JsonNode result = client.getForObject(url, JsonNode.class);
-    // TODO: Remove this tmp line
-    logger.info("Actuator health result:\n {}", result.toPrettyString());
     assertEquals("UP", result.path("status").asText());
   }
 
